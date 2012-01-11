90ab516ff870bd0f16a933ba04be4f579b2a6a54
Michael Freedman & Michael Schubert <pair+mfreedman+schubert@pivotallabs.com>
2012-01-11 17:48:20 -0500

    keyboard & mouse recipes will now notify the user that a logout is required at the end of deployment. these recipes will also use installation markers

--- recipes/keyboard_modifiers.rb
+++ recipes/keyboard_modifiers.rb
@@ -1,8 +1,15 @@
-mapping = ['<dict><key>HIDKeyboardModifierMappingDst</key><integer>2</integer><key>HIDKeyboardModifierMappingSrc</key><integer>0</integer></dict>']
-["1133-49221-0","1452-541-0","1452-544-0","1452-569-0"].each do |device|
-  plist ".GlobalPreferences" do
-    current_host true
-    key "com.apple.keyboard.modifiermapping.#{device}"
-    value mapping
+run_unless_marker_file_exists("keyboard_modifiers") do
+  include_recipe "casecommons_workstation::logout_marker"
+  mapping = ['<dict><key>HIDKeyboardModifierMappingDst</key><integer>2</integer><key>HIDKeyboardModifierMappingSrc</key><integer>0</integer></dict>']
+  ["1133-49221-0","1452-541-0","1452-544-0","1452-569-0"].each do |device|
+    plist ".GlobalPreferences" do
+      current_host true
+      key "com.apple.keyboard.modifiermapping.#{device}"
+      value mapping
+    end
+  end
+  execute "notify user of logout" do
+    command "true"
+    notifies :run, "execute[touch-logout-required-marker]", :immediately
   end
 end
--- recipes/scrolling.rb
+++ recipes/scrolling.rb
@@ -1,9 +1,18 @@
-plist ".GlobalPreferences" do
-  key "com.apple.swipescrolldirection"
-  value false
-end
+run_unless_marker_file_exists("scrolling_direction") do
+  include_recipe "casecommons_workstation::logout_marker"
+  plist ".GlobalPreferences" do
+    key "com.apple.swipescrolldirection"
+    value false
+  end
 
-plist "com.apple.driver.AppleBluetoothMultitouch.trackpad" do
-  key "TrackpadScroll"
-  value false
+  plist "com.apple.driver.AppleBluetoothMultitouch.trackpad" do
+    key "TrackpadScroll"
+    value false
+  end
+
+  execute "notify user of logout" do
+    command "true"
+    notifies :run, "execute[touch-logout-required-marker]", :immediately
+  end
+
 end
