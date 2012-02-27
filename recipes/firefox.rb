20e257aeb6deda948c3b9355c065b512735d7814
Dave Goddard & Sam Obukwelu <pair+dgoddard+sam@pivotallabs.com>
2012-02-27 13:44:00 -0500

    firefox version fix

--- recipes/firefox.rb
+++ recipes/firefox.rb
@@ -1,12 +1,16 @@
-check_firefox_version = "test '#{node[:firefox][:version]}' = $(/Applications/Firefox.app/Contents/MacOS/firefox -v | cut -f 3 -d ' ')"
+check_firefox_verison = %Q{test "Mozilla Firefox #{node[:firefox][:version]}" = "`/Applications/Firefox.app/Contents/MacOS/firefox -v`"}
 
 remote_file "/tmp/firefox-#{node[:firefox][:version]}.dmg" do
   source "http://download.mozilla.org/?product=firefox-#{node[:firefox][:version]}&os=osx&lang=en-US"
+  mode "0644"
   checksum node[:firefox][:checksum]
   not_if check_firefox_version
 end
 
 execute "install firefox" do
-  command "hdiutil attach /tmp/firefox-#{node[:firefox][:version]}.dmg && cp -R /Volumes/Firefox/Firefox.app /Applications/ ; hdiutil detach /Volumes/Firefox"
-  not_if check_firefox_version
+  command <<-SH
+    hdiutil attach /tmp/firefox-#{node[:firefox][:version]}.dmg
+    cp -R /Volumes/Firefox/Firefox.app /Applications/
+    hdiutil detach /Volumes/Firefox
+  SH
 end
