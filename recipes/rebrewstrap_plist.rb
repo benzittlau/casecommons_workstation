9847b5520b2e44e356602a4f81603170e54d4449
Matt Royal & Michael Freedman <pair+mroyal+mfreedman@pivotallabs.com>
2012-02-29 15:36:09 -0500

    updated rebrewstrap plist to reside in the correct directory

--- recipes/rebrewstrap_plist.rb
+++ recipes/rebrewstrap_plist.rb
@@ -1,9 +1,18 @@
-template "/Library/LaunchAgents/com.pivotal.rebrewstrap.plist" do
+template "/Library/LaunchDaemons/com.pivotal.rebrewstrap.plist" do
   source "com.pivotal.rebrewstrap.plist.erb"
   mode "0644"
 end
 
+execute "unload-deprecated-rebrewstrap-plist" do
+  only_if "test -f /Library/LaunchAgents/com.pivotal.rebrewstrap.plist"
+  command "launchctl unload -w /Library/LaunchAgents/com.pivotal.rebrewstrap.plist"
+end
+
+execute "remove-deprecated-rebrewstrap-plist" do
+  only_if "test -f /Library/LaunchAgents/com.pivotal.rebrewstrap.plist"
+  command "rm -f /Library/LaunchAgents/com.pivotal.rebrewstrap.plist"
+end
+
 execute "load rebrewstrap plist" do
-  cwd "/Library/LaunchAgents"
-  command "launchctl load com.pivotal.rebrewstrap.plist"
+  command "launchctl load -w /Library/LaunchDaemons/com.pivotal.rebrewstrap.plist"
 end
