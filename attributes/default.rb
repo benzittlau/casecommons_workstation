39226478adbc23734e3c5bba9ca5f4f9c5d2695d
Matt Royal & Michael Freedman <pair+mroyal+mfreedman@pivotallabs.com>
2012-02-29 17:31:08 -0500

    fixed some malarky with the rebrewstrap plist template

--- attributes/default.rb
+++ attributes/default.rb
@@ -14,3 +14,7 @@ default[:brewstrap][:src] = "https://raw.github.com/schubert/brewstrap/master/br
 
 default[:firefox][:version] = "10.0.2"
 default[:firefox][:checksum] = "3cac4e93900bb8a1f03c6cbfb9b24a84583f1476aa90d912b25a7d98464a57d8"
+
+
+default[:rebrewstrap_plist] = "/Library/LaunchDaemons/com.pivotal.rebrewstrap.plist"
+default[:rebrewstrap_plist_deprecated] = "/Library/LaunchAgents/com.pivotal.rebrewstrap.plist"
--- recipes/rebrewstrap_plist.rb
+++ recipes/rebrewstrap_plist.rb
@@ -4,19 +4,22 @@ template "/Library/LaunchDaemons/com.pivotal.rebrewstrap.plist" do
 end
 
 execute "unload-deprecated-rebrewstrap-plist" do
-  only_if "test -f /Library/LaunchAgents/com.pivotal.rebrewstrap.plist"
-  command "launchctl unload -w /Library/LaunchAgents/com.pivotal.rebrewstrap.plist"
+  only_if "test -f #{node[:rebrewstrap_plist_deprecated]}"
+  command "launchctl unload -w #{node[:rebrewstrap_plist_deprecated]}"
 end
 
 execute "remove-deprecated-rebrewstrap-plist" do
-  only_if "test -f /Library/LaunchAgents/com.pivotal.rebrewstrap.plist"
-  command "rm -f /Library/LaunchAgents/com.pivotal.rebrewstrap.plist"
+  only_if "test -f #{node[:rebrewstrap_plist_deprecated]}"
+  command "rm -f #{node[:rebrewstrap_plist_deprecated]}"
 end
 
 execute "unload rebrewstrap plist" do
-  command "launchctl unload -w /Library/LaunchDaemons/com.pivotal.rebrewstrap.plist"
+  # only if the launchctl task isn't currently running
+  not_if "sudo launchctl list | egrep \"^[0-9]+.*com.pivotal.rebrewstrap\""
+  command "launchctl unload -w #{node[:rebrewstrap_plist]}"
 end
 
 execute "load rebrewstrap plist" do
-  command "launchctl load -w /Library/LaunchDaemons/com.pivotal.rebrewstrap.plist"
+  not_if "launchctl list | grep com.pivotal.rebrewstrap"
+  command "launchctl load -w #{node[:rebrewstrap_plist]}"
 end
