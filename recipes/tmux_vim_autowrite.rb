7e9b68b191d1de7e716cd49c09c086a574e09cde
Joe Moore & Peter Jaros <pair+joe+pjaros@pivotallabs.com>
2012-04-11 12:26:42 -0400

    Bash tells Vims in Tmux to write themselves.

---
+++ recipes/tmux_vim_autowrite.rb
@@ -0,0 +1,9 @@
+template "#{WS_HOME}/.bash_profile_includes/autowrite-vim.sh" do
+  source "autowrite-vim.sh.erb"
+  owner WS_USER
+end
+
+template "#{WS_HOME}/.bash_profile_includes/preexec.sh.lib" do
+  source "preexec.sh.lib.erb"
+  owner WS_USER
+end
