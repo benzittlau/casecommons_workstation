89b907fced6d4412759468b2b948685ad33d31ad
Joey Geiger & Micah Young <pair+jgeiger+micah@pivotallabs.com>
2012-07-24 16:25:42 -0400

    Fix postgres recipe to allow one time run install
    Make sure postgres is shut down before install and
    will properly re-install if it was uninstalled in the
    same run.

--- recipes/postgres.rb
+++ recipes/postgres.rb
@@ -8,7 +8,7 @@ end
 
 run_unless_marker_file_exists("postgres") do
 
-  plist_pattern =  File.join('~', 'Library', 'LaunchAgents','*postgres*.plist')
+  plist_pattern = File.join(File.expand_path('~'), 'Library', 'LaunchAgents','*postgres*.plist')
   if Dir[plist_pattern].any?
     log "postgres plist found at #{plist_pattern}"
     execute "unload the plist (shuts down the daemon)" do
@@ -19,27 +19,12 @@ run_unless_marker_file_exists("postgres") do
     log "Did not find plist at #{plist_pattern} don't try to unload it"
   end
 
-  brew_remove "postgresql"
-
-#    blow away default image's data directory
-  directory "/usr/local/var/postgres" do
-    action :delete
-    recursive true
-  end
-
-  brew_install "postgresql", sha: node[:postgres][:sha]
-
-  execute "create the database" do
-    command %'initdb -U #{WS_USER} --encoding=utf8 --locale=en_US /usr/local/var/postgres'
+  execute "ensure postgres is shut down before uninstall" do
+    command "ps aux | grep postgres |grep -v grep| awk '{print $2}' | xargs kill"
     user WS_USER
   end
 
-  launch_agents_path = File.expand_path('.', File.join('~','Library', 'LaunchAgents'))
-  directory launch_agents_path do
-    action :create
-    recursive true
-    owner WS_USER
-  end
+  brew_remove "postgresql"
 
   execute "remove postgres plists from launchd" do
     command "launchctl list | cut -f 3 | grep postgres | xargs -I {} launchctl remove {}; true"
@@ -49,6 +34,30 @@ run_unless_marker_file_exists("postgres") do
   execute "remove the existing plists" do
     command %'rm -f ~/Library/LaunchAgents/*postgresql*.plist'
     user WS_USER
+  end
+
+  # blow away default image's data directory
+  directory "/usr/local/var/postgres" do
+    action :delete
+    recursive true
+  end
+
+  execute "brew install postgresql" do
+    not_if "brew list postgresql"
+    user WS_USER
+    command "brew install postgresql"
+  end
+
+  execute "initialize the database" do
+    command %'initdb -U #{WS_USER} --encoding=utf8 --locale=en_US /usr/local/var/postgres'
+    user WS_USER
+  end
+
+  launch_agents_path = File.expand_path('.', File.join('~','Library', 'LaunchAgents'))
+  directory launch_agents_path do
+    action :create
+    recursive true
+    owner WS_USER
   end
 
   execute "copy over the plist" do
@@ -90,4 +99,5 @@ script "restart-postgres-server" do
     launchctl load -w #{WS_HOME}/Library/LaunchAgents/*postgresql*.plist
   SH
 end
+
 
